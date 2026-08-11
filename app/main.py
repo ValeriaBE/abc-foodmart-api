d5c80174f7c9d2aed03ef62892d34b0d5cdee915
ValeriaBE <valeberroc@gmail.com>
2026-08-11 16:33:44 -0500

    Remove trailing slash

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://valeriabe.github.io/"
+        "https://valeriabe.github.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
